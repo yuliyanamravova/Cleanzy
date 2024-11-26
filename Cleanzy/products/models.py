a0a98b5851a7401905711269e0ea0864ff34d123
Yuliyana <yuliyanamravova@gmail.com>
2024-11-26 23:24:55 +0200

    added designs and more functionality

--- Cleanzy/products/models.py
+++ Cleanzy/products/models.py
@@ -34,7 +34,7 @@ class Product(models.Model):
     )
     price = models.DecimalField(decimal_places=2, max_digits=5)
     instructions = models.TextField()
-    group = models.ForeignKey(ProductGroups, on_delete=models.CASCADE)
+    """group = models.ForeignKey(ProductGroups, on_delete=models.CASCADE)"""
 
     def __str__(self):
         return self.name
